e97a767ea6577e218cdcd361b05661d2d6ed43b1
luo <lyw462680599@gmail.com>
2022-06-07 20:49:42 +0800

    master

---
+++ 时间的比较/main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+
+standard_file = pd.read_csv(r'D:\data_analyse\时间的比较\test_1.csv')
+aim_file = pd.read_csv(r'D:\data_analyse\时间的比较\test_2.csv')
+
+
+# 进行比较
+
+data_list = standard_file.loc[standard_file['time'] == aim_file['time']]
+
+print(data_list['name'])
+
+# 相同的时间段可以直接进行比较
